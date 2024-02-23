32afc729698de867695fb1b50d92737af27a4a64
Mats <>
2024-02-23 17:13:40 +0100

    Added class DeckOfCards

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/PlayingCard.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/PlayingCard.java
@@ -1,5 +1,7 @@
 package no.ntnu.idatx2003.oblig3.cardgame;
 
+
+// TODO: Check that JavaFX is working from the command line.
 /**
  * Represents a playing card. A playing card has a number (face) between
  * 1 and 13, where 1 is called an Ace, 11 = Knight, 12 = Queen and 13 = King.
